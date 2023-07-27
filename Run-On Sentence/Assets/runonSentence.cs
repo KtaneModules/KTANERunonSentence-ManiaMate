902ae70f76411c8a0b7f87a2a78b5c7da2c033c5
ManiaMate <69624454+ManiaMate@users.noreply.github.com>
2023-07-27 01:17:20 -0700

    Reset Butt/FirstCond

--- Run-On Sentence/Assets/runonSentence.cs
+++ Run-On Sentence/Assets/runonSentence.cs
@@ -20,12 +20,7 @@ public class runonSentence : MonoBehaviour
     public MeshRenderer TextRenderer;
     public MeshRenderer mask;
     public MaskShaderManager manage;
-    private float textSpeed = 0.1f;
-    int length;
-
-    float textPosBegin = 0.092f;
-    float boundaryTextEnd = -0.094f;
-    bool isLooping = true;
+    private float textSpeed = 0.05f;
 
     public MeshRenderer ScreenColorBkgd;
     public MeshRenderer ScreenShape;
@@ -35,10 +30,14 @@ public class runonSentence : MonoBehaviour
     public KMSelectable[] ColorButtons;
     public KMSelectable[] ShapeButtons;
     public KMSelectable[] PatternButtons;
+    public KMSelectable ResetButton;
 
     private char _currentColor;
     private char _currentPattern;
     private char _currentShape;
+    private List<string> conds = new List<string>();
+    private string tempMesg;
+    private List<int> possAnswers = new List<int>();
 
     private static readonly string _possibleColors = "WLDK";
     private static readonly string _possiblePatterns = "DEFS";
@@ -85,16 +84,15 @@ public class runonSentence : MonoBehaviour
 
     void Activate()
     { //Shit that should happen when the bomb arrives (factory)/Lights turn on
-        string[] sentence = { "Yo wassup hello, sit yo pretty ass so long as you came in the door, I just wanna chill.", "Hello" };
-        int index2 = Rnd.Range(0, 2);
-        text.text = sentence[index2];
+        //string[] sentence = { "Yo wassup hello, sit yo pretty ass so long as you came in the door, I just wanna chill.", "Hello" };
+        //int index2 = Rnd.Range(0, 2);
+        //text.text = sentence[index2];
         StartCoroutine(Scroll());
     }
 
     IEnumerator Scroll()
     {
         text.transform.localPosition = new Vector3(text.transform.localPosition.x, 0.0352f, text.transform.localPosition.z);
-
         while (true)
         {
             text.transform.Translate(Vector3.left * Time.deltaTime * textSpeed);
@@ -103,7 +101,7 @@ public class runonSentence : MonoBehaviour
     }
 
     void Start()
-    { //Shit
+    { 
         ModuleId = ModuleIdCounter++;
         GetComponent<KMBombModule>().OnActivate += Activate;
 
@@ -119,8 +117,9 @@ public class runonSentence : MonoBehaviour
             ShapeButtons[i].OnInteract += ShapeButtonPress(i);
         for (int i = 0; i < PatternButtons.Length; i++)
             PatternButtons[i].OnInteract += PatternButtonPress(i);
+        ResetButton.OnInteract += ResetButtonPress();
 
-        string sent = "The chosen cell is ";
+        /*
         int index = Rnd.Range(0, 64);
         Debug.Log(index);
         int row = index / 8;
@@ -128,9 +127,10 @@ public class runonSentence : MonoBehaviour
         char colorAnswer = colorChart[row, col];
         char shapeAnswer = shapeChart[row, col];
         char pattAnswer = patternChart[row, col];
-        // Debug.Log(colorAnswer);
-        // Debug.Log(shapeAnswer);
-        // Debug.Log(pattAnswer);
+        Debug.Log(colorAnswer);
+        Debug.Log(shapeAnswer);
+        Debug.Log(pattAnswer);
+        */
 
         _currentColor = _possibleColors.PickRandom();
         _currentShape = _possibleShapes.PickRandom();
@@ -138,6 +138,88 @@ public class runonSentence : MonoBehaviour
         Debug.Log(_currentColor + ", " + _currentShape + ", " + _currentPattern);
 
         UpdateDisplayedShape();
+
+        string[] beg = {"The chosen cell ", "The answer ", "The opposite of incorrect answer "};
+
+        for (int i = 0; i < 64; i++)
+        {
+            possAnswers.Add(i);
+        }
+
+        bool simpleCond = false;
+        string condition = GenerateSimpleCond(possAnswers);
+        Debug.Log(possAnswers.Count);
+
+        //Test for simple cond
+        text.text = beg[Rnd.Range(0, 3)] + condition;
+
+        /* while loop to keep going with conditions
+        while (possAnswers.Count > 1)
+        {
+            int tempcount = possAnswers.Count;
+            if (simpleCond == false && Rnd.Range(0, 5) == 4)
+            {
+                simpleCond = true;
+                tempMesg = GenerateSimpleCond(possAnswers);
+                if (tempcount != possAnswers.Count)
+                {
+                    conds.Add(tempMesg);
+                }
+            }
+            else
+            {
+                tempMesg = GenerateCompCond(possAnswers);
+                if (tempcount != possAnswers.Count)
+                {
+                    conds.Add(tempMesg);
+                }
+            }
+        }
+        */
+    }
+
+     private string GenerateSimpleCond(List<int> possAnswers)
+    {
+        int type = Rnd.Range(0, 3);
+        int specType = Rnd.Range(0, 4);
+        if(type == 0)
+        {
+            char ans = _possiblePatterns[specType];
+            for (int i = 0; i < 64; i++)
+            {
+                if (!(patternChart[i/8, i%8].Equals(ans)))
+                {
+                    possAnswers.Remove(i);
+                }
+            }
+            return "is " + ans;
+        }
+        if (type == 1)
+        {
+            char ans2 = _possibleShapes[specType];
+            for (int i = 0; i < 64; i++)
+            {
+                if (!(shapeChart[i/8, i%8].Equals(ans2)))
+                {
+                    possAnswers.Remove(i);
+                }
+            }
+            return "has a " + ans2;
+        }
+            char ans3 = _possibleColors[specType];
+            for (int i = 0; i < 64; i++)
+            {
+                if (!(colorChart[i/8, i%8].Equals(ans3)))
+                {
+                    possAnswers.Remove(i);
+                }
+            }
+            return "is on a " + ans3 + " background";
+    }
+
+    private void GenerateCompCond(List<int> possAnswers)
+    {
+       
     }
 
     private KMSelectable.OnInteractHandler ColorButtonPress(int i)
@@ -167,6 +249,15 @@ public class runonSentence : MonoBehaviour
             _currentPattern = _possiblePatterns[i];
             UpdateDisplayedShape();
             return false;
+        };
+    }
+
+    private KMSelectable.OnInteractHandler ResetButtonPress()
+    {
+        return delegate ()
+        {
+           text.transform.localPosition = new Vector3(text.transform.localPosition.x, 0.0352f, text.transform.localPosition.z);
+           return false;
         };
     }
 
