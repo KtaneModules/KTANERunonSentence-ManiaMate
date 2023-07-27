dcb99a79455d5cb84ba37be8a4be1b427546f4db
QuinnWuest <qwuest123@gmail.com>
2023-07-27 00:53:22 -0400

    Shape screen and shape buttons.

--- Run-On Sentence/Assets/runonSentence.cs
+++ Run-On Sentence/Assets/runonSentence.cs
@@ -27,6 +27,23 @@ public class runonSentence : MonoBehaviour
     float boundaryTextEnd = -0.094f;
     bool isLooping = true;
 
+    public MeshRenderer ScreenColorBkgd;
+    public MeshRenderer ScreenShape;
+    public Material[] ScreenColorMats;
+    public Texture[] ScreenTextures;
+
+    public KMSelectable[] ColorButtons;
+    public KMSelectable[] ShapeButtons;
+    public KMSelectable[] PatternButtons;
+
+    private char _currentColor;
+    private char _currentPattern;
+    private char _currentShape;
+
+    private static readonly string _possibleColors = "WLDK";
+    private static readonly string _possiblePatterns = "DEFS";
+    private static readonly string _possibleShapes = "OSTX";
+
     //W = white, K = black, D = darkgrey, L = lightgrey
     private char[,] colorChart = new char[,]
     {
@@ -69,7 +86,7 @@ public class runonSentence : MonoBehaviour
     void Activate()
     { //Shit that should happen when the bomb arrives (factory)/Lights turn on
         string[] sentence = { "Yo wassup hello, sit yo pretty ass so long as you came in the door, I just wanna chill.", "Hello" };
-        int index2 = UnityEngine.Random.Range(0, 2);
+        int index2 = Rnd.Range(0, 2);
         text.text = sentence[index2];
         StartCoroutine(Scroll());
     }
@@ -81,14 +98,12 @@ public class runonSentence : MonoBehaviour
         while (true)
         {
             text.transform.Translate(Vector3.left * Time.deltaTime * textSpeed);
-            Debug.Log(textSpeed);
             yield return null;
         }
     }
 
     void Start()
     { //Shit
-
         ModuleId = ModuleIdCounter++;
         GetComponent<KMBombModule>().OnActivate += Activate;
 
@@ -98,21 +113,68 @@ public class runonSentence : MonoBehaviour
         TextRenderer.material.mainTexture = fontTexture;
         mask.sharedMaterial = mr.Mask;
 
+        for (int i = 0; i < ColorButtons.Length; i++)
+            ColorButtons[i].OnInteract += ColorButtonPress(i);
+        for (int i = 0; i < ShapeButtons.Length; i++)
+            ShapeButtons[i].OnInteract += ShapeButtonPress(i);
+        for (int i = 0; i < PatternButtons.Length; i++)
+            PatternButtons[i].OnInteract += PatternButtonPress(i);
+
         string sent = "The chosen cell is ";
-        int index = UnityEngine.Random.Range(0, 64);
+        int index = Rnd.Range(0, 64);
         Debug.Log(index);
         int row = index / 8;
         int col = index % 8;
         char colorAnswer = colorChart[row, col];
         char shapeAnswer = shapeChart[row, col];
         char pattAnswer = patternChart[row, col];
-        Debug.Log(colorAnswer);
-        Debug.Log(shapeAnswer);
-        Debug.Log(pattAnswer);
+        // Debug.Log(colorAnswer);
+        // Debug.Log(shapeAnswer);
+        // Debug.Log(pattAnswer);
 
+        _currentColor = _possibleColors.PickRandom();
+        _currentShape = _possibleShapes.PickRandom();
+        _currentPattern = _possiblePatterns.PickRandom();
+        Debug.Log(_currentColor + ", " + _currentShape + ", " + _currentPattern);
 
+        UpdateDisplayedShape();
     }
 
+    private KMSelectable.OnInteractHandler ColorButtonPress(int i)
+    {
+        return delegate ()
+        {
+            _currentColor = _possibleColors[i];
+            UpdateDisplayedShape();
+            return false;
+        };
+    }
+
+    private KMSelectable.OnInteractHandler ShapeButtonPress(int i)
+    {
+        return delegate ()
+        {
+            _currentShape = _possibleShapes[i];
+            UpdateDisplayedShape();
+            return false;
+        };
+    }
+
+    private KMSelectable.OnInteractHandler PatternButtonPress(int i)
+    {
+        return delegate ()
+        {
+            _currentPattern = _possiblePatterns[i];
+            UpdateDisplayedShape();
+            return false;
+        };
+    }
+
+    private void UpdateDisplayedShape()
+    {
+        ScreenColorBkgd.material = ScreenColorMats[_possibleColors.IndexOf(_currentColor)];
+        ScreenShape.material.mainTexture = ScreenTextures[_possiblePatterns.IndexOf(_currentPattern) + (4 * _possibleShapes.IndexOf(_currentShape)) + ((_possibleColors.IndexOf(_currentColor) > 1) ? 16 : 0)];
+    }
 
     void Update()
     { //Shit that happens at any point after initialization
